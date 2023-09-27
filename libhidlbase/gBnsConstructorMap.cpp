c61ce14b25a520d8d0e2b7b1be49814a413b86d8
Dominik Baronelli <dbaronelli@shiftphones.com>
2023-09-27 16:01:25 +0200

    compat: libhidlbase: add shim for missing constructor map symbol
    gBn/sConstructorMap symbols were removed from libhidl:
    - https://android.googlesource.com/platform/system/libhidl/+/e29b2ad
    
    However, some prebuilts still make use of them.
    
    In some cases, for example when using GSI, libhidl can not be patched
    directly, which requires modifying affected blobs to depend on this shim
    library.
    
    Change-Id: I097c75ac210a9c3f50a9725da0b910d819c85b9a
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- libhidlbase/gBnsConstructorMap.cpp
+++ libhidlbase/gBnsConstructorMap.cpp
@@ -1,9 +1,12 @@
+<<<<<<< HEAD
 /*
  * Copyright (C) 2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+=======
+>>>>>>> 5f10869 (compat: libhidlbase: add shim for missing constructor map symbol)
 #include <hidl/Static.h>
 
 namespace android {
@@ -16,6 +19,12 @@ DoNotDestruct<BnConstructorMap> gBnConstructorMap{};
 // Deprecated; kept for ABI compatibility. Use getBsConstructorMap.
 DoNotDestruct<BsConstructorMap> gBsConstructorMap{};
 
+<<<<<<< HEAD
 }  // namespace details
 }  // namespace hardware
 }  // namespace android
+=======
+} // namespace details
+} // namespace hardware
+} // namespace android
+>>>>>>> 5f10869 (compat: libhidlbase: add shim for missing constructor map symbol)
