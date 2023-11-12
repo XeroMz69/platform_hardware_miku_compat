b160c7ec424e4b10fb8c7d11dad7f5f6248edb94
LuK1337 <priv.luk@gmail.com>
2023-11-12 16:13:29 +0100

    libhidlbase: Run clang format
    Change-Id: I8bff92f14ae582e4208a7b3e5c4aaec28a883b76

--- libhidlbase/gBnsConstructorMap.cpp
+++ libhidlbase/gBnsConstructorMap.cpp
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 /*
  * Copyright (C) 2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-=======
->>>>>>> 5f10869 (compat: libhidlbase: add shim for missing constructor map symbol)
 #include <hidl/Static.h>
 
 namespace android {
@@ -19,12 +16,6 @@ DoNotDestruct<BnConstructorMap> gBnConstructorMap{};
 // Deprecated; kept for ABI compatibility. Use getBsConstructorMap.
 DoNotDestruct<BsConstructorMap> gBsConstructorMap{};
 
-<<<<<<< HEAD
 }  // namespace details
 }  // namespace hardware
 }  // namespace android
-=======
-} // namespace details
-} // namespace hardware
-} // namespace android
->>>>>>> 5f10869 (compat: libhidlbase: add shim for missing constructor map symbol)
