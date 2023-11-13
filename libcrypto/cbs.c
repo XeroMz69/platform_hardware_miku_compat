097adea85b449246f9d12d0c3772db729995bc87
LuK1337 <priv.luk@gmail.com>
2023-11-13 13:19:43 +0100

    libhidlbase: Add SPDX license
    Change-Id: Ic5cc6616bc2b7ca45f7ad2bca25fa494650a69b7

--- libcrypto/cbs.c
+++ libcrypto/cbs.c
@@ -4,24 +4,13 @@
  * SPDX-License-Identifier: ISC
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include <openssl/base.h>
-=======
 #include <stdint.h>
->>>>>>> 969158f (compat: Add libcrypto_shim)
-=======
 #include <openssl/base.h>
->>>>>>> f029617 (compat: Add libcrypto_shim)
 
 // From <openssl/bytestring.h>
 struct cbs_st {
     const uint8_t* data;
     size_t len;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f029617 (compat: Add libcrypto_shim)
 
 #if !defined(BORINGSSL_NO_CXX)
     // Allow implicit conversions to and from bssl::Span<const uint8_t>.
@@ -37,15 +26,10 @@ struct cbs_st {
 #endif
 };
 
-<<<<<<< HEAD
-=======
 };
 
 typedef struct cbs_st CBS;
 
->>>>>>> 969158f (compat: Add libcrypto_shim)
-=======
->>>>>>> f029617 (compat: Add libcrypto_shim)
 void CBS_init(CBS* cbs, const uint8_t* data, size_t len) {
     cbs->data = data;
     cbs->len = len;
